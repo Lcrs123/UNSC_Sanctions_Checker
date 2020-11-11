f2e53e69926fcb7aa0d607308b54ad71bf714efd
Lucas <lucascamillo333@hotmail.com>
2020-11-11 17:54:30 -0300

    Put everything into Application class.
    Created auto-download and check for update functions.

--- UNSC Sanctions.py
+++ UNSC Sanctions.py
@@ -2,8 +2,8 @@ import pandas as pd
 import xml.etree.ElementTree as et
 from fuzzywuzzy import process
 from tkinter import *
-from tkinter import filedialog,ttk
-from os import getcwd
+from tkinter import filedialog,ttk, messagebox,simpledialog
+from os import getcwd, listdir, stat
 import requests
 
 def get_column_list(element) -> list:
@@ -14,25 +14,117 @@ def get_column_list(element) -> list:
         element_columns.append(column.tag)
     return element_columns
 
-def append_individuals_full_names(df):
-    df = df.fillna(value={'FIRST_NAME': ' ', 'SECOND_NAME': ' ','THIRD_NAME': ' '})
-    df[['FIRST_NAME', 'SECOND_NAME', 'THIRD_NAME']] = df[['FIRST_NAME', 'SECOND_NAME', 'THIRD_NAME']].astype(str)
-    df['FULL_NAME'] = df['FIRST_NAME'] + df['SECOND_NAME'] + df['THIRD_NAME']
-    return df
-
 class Application():
     def __init__(self):
         self.list = None
+        self.list_found = False
         self.elements_list = []
         self.individuals_df = None
         self.entities_df = None
+        self.root = Tk()
+        self.list_path = 'consolidated.xml'
+
+    def create_frame(self):
+        self.mainframe = ttk.Frame(self.root, padding='4 4 20 20')
+        self.mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
+        self.root.columnconfigure(0, weight=1)
+        self.root.rowconfigure(0, weight=1)
+
+    def create_load_button(self):
+        ttk.Button(self.mainframe, text='Load list', command=self.load_button).grid(
+            column=0, row=0, sticky=(N, W))
+
+    def create_get_list_button(self):
+        ttk.Button(self.mainframe, text="Get list online", command=self.get_list_button).grid(column=1,row=0)
+
+    def connect_to_link(self,link="https://scsanctions.un.org/resources/xml/en/consolidated.xml"):
+        r = requests.get(link)
+        if r.status_code == 200 and r.headers['Content-Type'].__contains__(
+            'xml') and r.text.__contains__('CONSOLIDATED_LIST'):
+            return r
+        else:
+            if messagebox.askyesno(message=(
+            f'Could not find the list at {link}\nWould you like to try with a different link?')):
+                self.connect_to_link(link=simpledialog.askstring(
+                    prompt=f'Please enter new adress for UNSC Sanctions list in xml format\n Last address used:{link}'))
+
+    def create_check_update_button(self):
+        ttk.Button(self.mainframe, text="Check for update",
+                   command=self.check_update_button).grid(column=2, row=0)
+
+    def check_list_is_outdated(self,requests_object):
+        if int(requests_object.headers['Content-Length']) == stat(path=self.list_path)[-4]:
+            return False
+        else:
+            return True
+
+    def check_update_button(self):
+        r = self.connect_to_link()
+        if self.check_list_is_outdated(r):
+            if messagebox.askyesno(message='Current list is outdated, download new one?'):
+                self.get_list_button()
+        else:
+            messagebox.showinfo(message='List is up-to-date')
+
+
+    def save_downloaded_list(self, requests_object):
+        with open('consolidated.xml', 'wb') as fd:
+            for chunk in requests_object.iter_content(chunk_size=128):
+                fd.write(chunk)
+        messagebox.showinfo(message=f'List downloaded from {requests_object.url} and saved as consolidated.xml at {getcwd()}')
+
+
+    def get_list_button(self):
+        r = self.connect_to_link()
+        self.save_downloaded_list(r)
+        self.autoload_list()
+
+    def create_name_entry(self):
+        self.name = StringVar()
+        self.name_entry = ttk.Entry(self.mainframe, width=24, textvariable=self.name)
+        self.name_entry.grid(column=2, row=1, sticky=(W, E))
+        ttk.Label(self.mainframe, text='Name:').grid(column=2, row=0, sticky=W)
+
+    def create_score_entry(self):
+        self.score = IntVar(value=100)
+        self.score_entry = ttk.Entry(self.mainframe, width=3,textvariable=self.score)
+        self.score_entry.grid(column=4, row=1)
+        ttk.Label(self.mainframe, text='Score').grid(column=3,row=1)
+
+    def create_search_button(self):
+        ttk.Button(self.mainframe, text='Search name', command=self.search_button).grid(column=2,row=2, sticky=(N, W))
+
+    def create_matches_list(self):
+        self.matches = StringVar()
+        ttk.Label(self.mainframe, text='Matches:').grid(column=1, row=3,
+                                                        sticky=W)
+        ttk.Label(self.mainframe, textvariable=self.matches).grid(column=1, row=4,
+                                                        sticky=S)
+
+    def check_for_list(self):
+        if "consolidated.xml" in listdir(getcwd()):
+            return True
+        else:
+            return False
+
+    def autoload_list(self):
+        if self.check_for_list():
+            self.list = et.parse("consolidated.xml")
+            root = self.list.getroot()
+            self.elements_list.extend(list(root)[0:2])
+            self.make_element_dfs()
+            self.clean_individuals_df()
+            self.append_individuals_full_name()
+            messagebox.showinfo(message=f"Loaded list from file 'consolidated.xml' in dir {getcwd()}")
+        else:
+            messagebox.showinfo(message="Could not find list file ('consolidated.xml') in current dir\nPlease load the list manually or get it from the UNSC website")
 
     def load_list(self):
         path_window = Tk()
-        xml_list_path = filedialog.askopenfilename(initialdir=getcwd(), filetypes=[('Xml files','*.xml')], title='Choose xml list')
+        self.list_path = filedialog.askopenfilename(initialdir=getcwd(), filetypes=[('Xml files','*.xml')], title='Choose xml list')
         path_window.withdraw()
         path_window.destroy()
-        self.list = et.parse(xml_list_path)
+        self.list = et.parse(self.list_path)
         root = self.list.getroot()
         self.elements_list.extend(list(root)[0:2])
 
@@ -66,41 +158,30 @@ class Application():
         self.clean_individuals_df()
         self.append_individuals_full_name()
 
-def search_button():
-    name = str(name_entry.get())
-    matches.set(str(process.extractBests(query=name, choices=app.individuals_df['FULL_NAME'],score_cutoff=score.get())).replace(',','\n'))
+    def search_button(self):
+        name = str(self.name_entry.get())
+        self.matches.set(str(process.extractBests(query=name,
+                                             choices=self.individuals_df[
+                                                 'FULL_NAME'],
+                                             score_cutoff=self.score.get())).replace(
+            ',', '\n'))
+
+    def main(self):
+        self.root.title('UNSC Sanctions Checker')
+        self.create_frame()
+        self.create_load_button()
+        self.create_name_entry()
+        self.create_score_entry()
+        self.create_search_button()
+        self.create_matches_list()
+        self.create_get_list_button()
+        self.create_check_update_button()
+        self.autoload_list()
+        self.root.mainloop()
+
 
 
 if __name__ == '__main__':
     app = Application()
-    root = Tk()
-    root.title('UNSC Sanctions Checker')
+    app.main()
 
-    mainframe = ttk.Frame(root, padding = '4 4 20 20')
-    mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
-    root.columnconfigure(0, weight=1)
-    root.rowconfigure(0, weight=1)
-
-    ttk.Button(mainframe, text='Load list', command=app.load_button).grid(column=0,row=0, sticky=(N, W))
-
-    name = StringVar()
-    name_entry = ttk.Entry(mainframe, width=24, textvariable=name)
-    name_entry.grid(column=2, row=1, sticky=(W, E))
-    ttk.Label(mainframe, text='Name:').grid(column=2,row=0, sticky=W)
-
-    score = IntVar(value=100)
-    score_entry = ttk.Entry(mainframe, width=3,textvariable=score)
-    score_entry.grid(column=4, row=1)
-    ttk.Label(mainframe, text='Score').grid(column=3,row=1)
-
-    ttk.Button(mainframe, text='Search name', command=search_button).grid(column=2,row=2, sticky=(N, W))
-
-    matches = StringVar()
-    ttk.Label(mainframe, textvariable=matches).grid(column=1, row=4, sticky=S)
-    ttk.Label(mainframe, text='Matches:').grid(column=1,row=3,sticky=W)
-
-
-
-    name_entry.focus()
-
-    root.mainloop()
