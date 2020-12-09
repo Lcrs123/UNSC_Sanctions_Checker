a8b49030907e068b5c49d504f87ffbf427ae53d1
Lucas <lucascamillo333@hotmail.com>
2020-12-08 23:52:16 -0300

    adapting to heroku

--- unsc/checker/admin.py
+++ unsc/checker/admin.py
@@ -6,29 +6,4 @@ from .models import ENTITIES,INDIVIDUALS
 admin.site.register(INDIVIDUALS)
 admin.site.register(ENTITIES)
 
-# @admin.register(INDIVIDUALS)
-# class IndividualAdmin(admin.ModelAdmin):
-#     list_display = ('FULL_NAME',
-#                     'DATAID',
-#                     'VERSIONNUM',
-#                     'UN_LIST_TYPE',
-#                     'REFERENCE_NUMBER',
-#                     'LISTED_ON',
-#                     'COMMENTS1',
-#                     'NAME_ORIGINAL_SCRIPT',
-#                     'GENDER',
-#                     'SUBMITTED_BY')
-#
-#
-# @admin.register(ENTITIES)
-# class EntitiesAdmin(admin.ModelAdmin):
-#     list_display = ("FIRST_NAME",
-#                     "DATAID",
-#                     "VERSIONNUM",
-#                     "UN_LIST_TYPE",
-#                     "REFERENCE_NUMBER",
-#                     "LISTED_ON",
-#                     "COMMENTS1",
-#                     "NAME_ORIGINAL_SCRIPT",
-#                     "SUBMITTED_ON")
 
