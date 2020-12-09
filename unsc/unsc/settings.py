a5bdcf5ff9922182b282a086f97a8f5cad4dbbb6
Lucas <lucascamillo333@hotmail.com>
2020-12-08 22:50:09 -0300

    adapting to django

--- unsc/unsc/settings.py
+++ unsc/unsc/settings.py
@@ -132,5 +132,5 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # django_heroku.settings(locals())
 
-# db_from_env = dj_database_url.config(conn_max_age=500)
-# DATABASES['default'].update(db_from_env)
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
